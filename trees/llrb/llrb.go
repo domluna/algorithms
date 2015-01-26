4249d36655ac8ccd352dae7c83e5477c64bb01e6
Dominique Luna <dluna132@gmail.com>
2015-01-25 20:25:30 -0500

    start of llrb done

--- trees/llrb/llrb.go
+++ trees/llrb/llrb.go
@@ -1,4 +1,6 @@
 // Left-leaning Red-black Tree http://www.cs.princeton.edu/~rs/talks/LLRB/LLRB.pdf
+// Based on 2-3 Trees.
+// TODO: implement delete
 package llrb
 
 // Color specifies the color of a node.
@@ -8,6 +10,16 @@ const (
 	Red   Color = false
 	Black Color = true
 )
+
+// Key represents a key in in a LLRB tree. A Key
+// must be comparable
+type Key interface {
+	// Less returns true if a < b
+	Less(a, b Key) bool
+
+	// Equal returns true if a == b
+	Equal(a, b Key) bool
+}
 
 // Node represents a node in the LLRB.
 type Node struct {
@@ -22,6 +34,7 @@ type Tree struct {
 	root *Node
 }
 
+// New creates a new LLRB Tree.
 func New() *Tree {
 	return &Tree{
 		root: nil,
@@ -42,6 +55,24 @@ func (t *Tree) Search(key int) interface{} {
 	return nil
 }
 
+func (t *Tree) Height() int {
+	return height(t.root) + 1
+}
+
+func height(n *Node) int {
+	if n == nil {
+		return 0
+	}
+
+	leftHeight := height(n.left)
+	rightHeight := height(n.right)
+
+	if leftHeight > rightHeight {
+		return leftHeight + 1
+	}
+	return rightHeight + 1
+}
+
 func (t *Tree) Insert(key int, value interface{}) {
 	t.root = insert(t.root, key, value)
 	t.root.Color = Black
@@ -56,10 +87,6 @@ func insert(n *Node, key int, value interface{}) *Node {
 		}
 	}
 
-	if isRed(n.left) && isRed(n.right) {
-		colorFlip(n)
-	}
-
 	if key == n.Key {
 		n.Value = value
 	} else if key < n.Key {
@@ -68,12 +95,16 @@ func insert(n *Node, key int, value interface{}) *Node {
 		n.right = insert(n.right, key, value)
 	}
 
-	if isRed(n.right) && !isRed(n.right) {
+	if isRed(n.right) && !isRed(n.left) {
 		n = rotateLeft(n)
 	}
 
 	if isRed(n.left) && isRed(n.left.left) {
 		n = rotateRight(n)
+	}
+
+	if isRed(n.left) && isRed(n.right) {
+		colorFlip(n)
 	}
 
 	return n
@@ -112,7 +143,7 @@ func colorFlip(n *Node) {
 
 }
 
-func rotateLeft(n *Node) *Node {
+func rotateRight(n *Node) *Node {
 	x := n.left
 	n.left = x.right
 	x.right = n
@@ -121,7 +152,7 @@ func rotateLeft(n *Node) *Node {
 	return x
 }
 
-func rotateRight(n *Node) *Node {
+func rotateLeft(n *Node) *Node {
 	x := n.right
 	n.right = x.left
 	x.left = n
@@ -150,5 +181,16 @@ func moveRedRight(n *Node) *Node {
 }
 
 func fixUp(n *Node) *Node {
+	if isRed(n.right) && !isRed(n.left) {
+		n = rotateLeft(n)
+	}
+
+	if isRed(n.left) && isRed(n.left.left) {
+		n = rotateRight(n)
+	}
+
+	if isRed(n.left) && isRed(n.right) {
+		colorFlip(n)
+	}
 	return n
 }
--- trees/llrb/llrb_test.go
+++ trees/llrb/llrb_test.go
@@ -1,8 +1,22 @@
 package llrb_test
 
-import "testing"
-import . "llrb"
+import (
+	"fmt"
+	"math/rand"
+	"testing"
 
-func TestDepth(t *testing.T) {
-	llrb.New()
+	"github.com/domluna/algorithms/trees/llrb"
+)
+
+func TestHeight(t *testing.T) {
+	tree := llrb.New()
+
+	nNodes := 1 << 20
+	for i := 0; i < nNodes; i++ {
+		n := rand.Int()
+		tree.Insert(n, n)
+	}
+
+	h := tree.Height()
+	fmt.Printf("Height of LLRB: %d, Log2: %d\n", h, 20)
 }
